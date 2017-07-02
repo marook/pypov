da87882c13c901007819d2154d7ec6828e75c8f6
Markus Peröbner <markus.peroebner@gmail.com>
2017-07-02 12:24:36 +0200

    adds initial skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pypov',
+      version='0.1.0',
+      license='GPLv3',
+      description='povray scene description DOM for python',
+      author='Markus Peroebner',
+      author_email='markus.peroebner@gmail.com',
+      url='https://github.com/marook/pypov',
+      packages=['pypov'],
+)
